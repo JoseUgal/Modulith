95fc77886a1d75c7065a19d32ea49801196d60ae
Jose Ugal <joseugal17@icloud.com>
2025-11-12 17:35:08 +0100

    feat: add value object base

---
+++ src/Common/Domain/Primitives/ValueObject.cs
@@ -0,0 +1,58 @@
+﻿namespace Domain.Primitives;
+
+/// <summary>
+/// Represents the abstract value object primitive.
+/// Provides equality and hash code implementations based on atomic values.
+/// </summary>
+public abstract class ValueObject : IEquatable<ValueObject>
+{
+    public static bool operator ==(ValueObject? a, ValueObject? b)
+    {
+        if (ReferenceEquals(a, b))
+        {
+            return true;
+        }
+
+        if (a is null && b is null)
+        {
+            return true;
+        }
+
+        if (a is null || b is null)
+        {
+            return false;
+        }
+
+        return a.Equals(b);
+    }
+
+    public static bool operator !=(ValueObject? a, ValueObject? b) => !(a == b);
+
+    /// <inheritdoc />
+    public virtual bool Equals(ValueObject? other) => other is not null && ValuesAreEqual(other);
+
+    /// <inheritdoc />
+    public override bool Equals(object? obj) => obj is ValueObject valueObject && ValuesAreEqual(valueObject);
+
+    /// <inheritdoc />
+    public override int GetHashCode()
+    {
+        var hash = new HashCode();
+
+        foreach (var value in GetAtomicValues())
+        {
+            hash.Add(value);
+        }
+
+        return hash.ToHashCode();
+    }
+
+    /// <summary>
+    /// Returns the atomic values of the value object used for equality and hashing.
+    /// Must not include mutable objects.
+    /// </summary>
+    /// <returns>An enumerable of atomic values representing the object.</returns>
+    protected abstract IEnumerable<object> GetAtomicValues();
+
+    private bool ValuesAreEqual(ValueObject valueObject) => GetAtomicValues().SequenceEqual(valueObject.GetAtomicValues());
+}
